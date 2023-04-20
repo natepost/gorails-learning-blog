cda13ccc82bc59c772db11809aee77b45d357a4d
natepost <nathanielpost@gmail.com>
2023-04-20 11:52:00 -0400

    setup crud basics on blogs

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
-  resources :names
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  # get "/blog_posts/new", to: "blog_posts#new", as: new_blog_post
+  # get "/blog_posts/:id", to: "blog_posts#show", as: blog_post
+
+  # post 
+  # put
+  # patch
+  # delete
+
+  resources :blog_posts
+  resources :names
   
   root "blog_posts#index"
 end
