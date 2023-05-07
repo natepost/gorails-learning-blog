4ea3f42d64232118ee4bdeb52cb79bdf04b0a4cf
natepost <nathanielpost@gmail.com>
2023-05-07 11:30:59 -0400

    add published at for draft posts

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,8 +1,9 @@
 class BlogPostsController < ApplicationController
+    before_action :authenticate_user!, except: [:index, :show]
     before_action :set_blog_post, except: [:index, :new, :created]
 
     def index
-        @blog_posts = BlogPost.all
+        @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
     end
 
     def show
@@ -42,12 +43,16 @@ class BlogPostsController < ApplicationController
 
     private
     def set_blog_post
-        @blog_post = BlogPost.find(params[:id])
+        if user_signed_in?
+            @blog_post = BlogPost.find(params[:id])
+        else
+            @blog_post = BlogPost.published.find(params[:id])
+        end
     rescue ActiveRecord::RecordNotFound
         redirect_to root_path
     end
 
     def blog_post_params
-        params.require(:blog_post).permit(:title, :body)
+        params.require(:blog_post).permit(:title, :body, :published_at)
     end
 end
